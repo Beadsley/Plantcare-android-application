ddd734d49b70faf97ababe7744a9687b792978fe
Beadsley <danbeadleson@gmail.com>
2019-03-30 16:39:30 +0100

    light and water requirements sepreated into sperate Textviews

--- app/src/main/java/com/example/myplants/PlantInfoActivity.java
+++ app/src/main/java/com/example/myplants/PlantInfoActivity.java
@@ -6,17 +6,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class PlantInfoActivity extends AppCompatActivity implements PlantsFragment.PlantListener {
     private static final String TAG= "PlantInfo";
-    TextView plantDetails;
+    TextView lightRequirementDetails;
+    TextView waterRequirementDetails;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_info);
         Log.v(TAG, "Plant Info Initialised");
-        plantDetails=findViewById(R.id.plant_details);
+        lightRequirementDetails=findViewById(R.id.light_requirements);
+        waterRequirementDetails=findViewById(R.id.water_requirements);
 
     }
     // sends the user back to the main menu when the go back button is selected
@@ -30,7 +31,9 @@ public class PlantInfoActivity extends AppCompatActivity implements PlantsFragme
     @Override
     public void onPlantSelected(int index) {
         Log.v(TAG,"selcted index: "+index);
-        String [] plantDescriptions= getResources().getStringArray(R.array.plantDescriptions);
-        plantDetails.setText(plantDescriptions[index]);
+        String [] lightRequirements= getResources().getStringArray(R.array.lightRequirements);
+        lightRequirementDetails.setText(lightRequirements[index]);
+        String [] waterRequirements= getResources().getStringArray(R.array.waterRequirements);
+        waterRequirementDetails.setText(waterRequirements[index]);
     }
 }
