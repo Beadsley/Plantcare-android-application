4d33ef669ad55040f331ac5ff34dbfb15d575547
Beadsley <danbeadleson@gmail.com>
2019-04-02 07:24:37 +0200

    Notifications added -User is able to enable a water reminder at a specified time

--- app/src/main/java/com/example/myplants/SettingsActivity.java
+++ app/src/main/java/com/example/myplants/SettingsActivity.java
@@ -1,34 +1,95 @@
 package com.example.myplants;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class SettingsActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+    private static final String TAG= "PlantSettings";
     Switch notificationsSwitch;
     Button submit;
+    Boolean alarmSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.v(TAG, "Alarm Set: "+alarmSet);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
         notificationsSwitch= findViewById(R.id.switch_notifications);
         submit=findViewById(R.id.btn_submit);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(notificationsSwitch.isChecked()){
-                    Toast.makeText(SettingsActivity.this, "Checked baby", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(SettingsActivity.this, "Checked baby", Toast.LENGTH_SHORT).show();
+
+                    DialogFragment timePicker = new TimePickerFragment();
+                    timePicker.show(getSupportFragmentManager(), "time picker");
+                    notificationsSwitch.setChecked(true);
+                    
+
                 }
                 else {
-                    Toast.makeText(SettingsActivity.this, "Not checked", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(SettingsActivity.this, "Not checked", Toast.LENGTH_SHORT).show();
+                    cancelAlarm();
+                    //notificationsSwitch.setChecked(false);
                 }
             }
         });
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, 0);
+
+        //Seperate Method ?
+        String timeText = "Alarm set for: ";
+        timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(calendar.getTime());
+        Toast.makeText(this, timeText, Toast.LENGTH_SHORT).show();
+
+        startAlarm(calendar);
+
+    }
+
+    private void startAlarm(Calendar calendar){
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+
+    }
+
+    private void cancelAlarm() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        alarmManager.cancel(pendingIntent);
+        Toast.makeText(this, "Reminder Cancelled", Toast.LENGTH_SHORT).show();
     }
 
     public void onGoBack(View view) {
