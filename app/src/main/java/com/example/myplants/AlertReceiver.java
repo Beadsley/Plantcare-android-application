65b074f4f3e38733c6b29dbc25c1f3f8f0808427
Beadsley <danbeadleson@gmail.com>
2019-04-04 11:23:19 +0200

    Descriptions added to the Main, broadcaster and favourites activity

--- app/src/main/java/com/example/myplants/AlertReceiver.java
+++ app/src/main/java/com/example/myplants/AlertReceiver.java
@@ -1,10 +1,17 @@
 package com.example.myplants;
-
+/**
+ *
+ * Class allows the Android system to receive
+ * an event from the application
+ *
+ * @author Anastasija Gurejeva
+ * @author Daniel Beadleson
+ * @author Mahlet Mulu
+ */
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
-
 import com.example.myplants.NotificationHelper;
 
 public class AlertReceiver extends BroadcastReceiver {
